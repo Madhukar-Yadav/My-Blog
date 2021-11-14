1d64a317cca827605e7f021d85dd2b074cd9a38d
Madhukar Yadav B <madhukar@ALIPL1054.local>
2021-11-14 15:39:31 +0530

    Hiding the Learn more anchor tag

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Hello there! Welcome to my blog.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
